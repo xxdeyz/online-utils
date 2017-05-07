5ea1e91172610c11c63eff4c65a733b629c63444
xxdeyz <ywclub@163.com>
2017-05-07 21:06:40 +0800

    Add PID file writer

--- online-utils/src/main/java/com/ex/onlineutils/OnlineUtilsApplication.java
+++ online-utils/src/main/java/com/ex/onlineutils/OnlineUtilsApplication.java
@@ -2,11 +2,14 @@ package com.ex.onlineutils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class OnlineUtilsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineUtilsApplication.class, args);
+		SpringApplication application = new SpringApplication(OnlineUtilsApplication.class, args);
+		application.addListeners(new ApplicationPidFileWriter("./bin/app.pid"));
+		application.run();
 	}
 }
